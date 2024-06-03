76e7170465aaaee81f9e655dd4423d92f5ec1e35
Kirill Guzovskiy <kirillguzovskiy@MacBook-Pro-Kirill-2.local>
2024-06-03 11:03:24 +0300

    add main.js

--- main.js
+++ main.js
@@ -8,20 +8,15 @@ const WEB_APP_BASE_URL = 'https://tgappbrighfairy.web.app';
 bot.start((ctx) => {
     const user = ctx.message.from;
     const telegramId = user.id;
-    const username = user.username;
-
-    if (username) {
-        console.log(`Информация о пользователе ${username} сохранена в Firebase`);
-        const uniqueLink = `${WEB_APP_BASE_URL}?telegramId=${telegramId}&username=${username}`;
-        console.log('Unique link generated:', uniqueLink); // Отладочный лог
-        ctx.reply('Добро пожаловать в магический мир Криптоленда! Чтобы начать путешествие, нажмите на кнопку «Магия»!', 
-            Markup.keyboard([
-                Markup.button.webApp('Магия!', uniqueLink)
-            ]).resize()
-        );
-    } else {
-        ctx.reply('У тебя нет username.');
-    }
+    const username = user.username || ''; // Используем пустую строку, если username отсутствует
+    const uniqueLink = username ? 
+        `${WEB_APP_BASE_URL}?telegramId=${telegramId}&username=${username}` :
+        `${WEB_APP_BASE_URL}?telegramId=${telegramId}`;
+    ctx.reply('Добро пожаловать в магический мир Криптоленда! Чтобы начать путешествие, нажмите на кнопку «Магия»!', 
+        Markup.keyboard([
+            Markup.button.webApp('Магия!', uniqueLink)
+        ]).resize()
+    );
 });
 
 bot.launch();
