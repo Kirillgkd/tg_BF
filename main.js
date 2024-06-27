b177fe60e1bb527b020275d3940232403c31a77e
Kirill Guzovskiy <kirillguzovskiy@MacBook-Pro-Kirill-2.local>
2024-06-27 21:47:52 +0300

    Обновление проекта

--- main.js
+++ main.js
@@ -1,53 +1,39 @@
 import { Telegraf, Markup } from 'telegraf';
-import { URLSearchParams } from 'url';
 
 // Инициализация Telegram бота
-const bot = new Telegraf('6979302839:AAFn6yPdRu17MwLv4A40JAgueZznSQJ6ShE'); // Замените на ваш токен
+const bot = new Telegraf('6979302839:AAFn6yPdRu17MwLv4A40JAgueZznSQJ6ShE');
 const WEB_APP_BASE_URL = 'https://tgappbrighfairy.web.app';
 
+// Обработка команды /start
 bot.start((ctx) => {
     const user = ctx.message.from;
     const telegramId = user.id;
     const username = user.username || '';
-    const uniqueLink = username ? 
-        `${WEB_APP_BASE_URL}?telegramId=${telegramId}&username=${username}` :
-        `${WEB_APP_BASE_URL}?telegramId=${telegramId}`;
-    ctx.reply('Добро пожаловать в магический мир Криптоленда! Чтобы начать путешествие, нажмите на кнопку «Магия»!', 
+    
+    // Извлечение startapp параметра
+    const startapp = ctx.message.text.split(' ')[1];
+    const referralIdMatch = startapp ? startapp.match(/^referralId_(\d+)$/) : null;
+    const referralId = referralIdMatch ? referralIdMatch[1] : '';
+
+    // Генерация уникальной ссылки
+    let uniqueLink;
+    if (referralId) {
+        uniqueLink = username ?
+            `${WEB_APP_BASE_URL}?telegramId=${telegramId}&username=${username}&referralId=${referralId}` :
+            `${WEB_APP_BASE_URL}?telegramId=${telegramId}&referralId=${referralId}`;
+    } else {
+        uniqueLink = username ?
+            `${WEB_APP_BASE_URL}?telegramId=${telegramId}&username=${username}` :
+            `${WEB_APP_BASE_URL}?telegramId=${telegramId}`;
+    }
+
+    // Отправка сообщения пользователю
+    ctx.reply('Добро пожаловать в магический мир Криптоленда! Чтобы начать путешествие, нажмите на кнопку «Магия»!',
         Markup.keyboard([
             Markup.button.webApp('Магия!', uniqueLink)
         ]).resize()
     );
 });
 
-// Обработка реферальных ссылок
-bot.on('message', (ctx) => {
-    const message = ctx.message;
-    
-    if (message.entities && message.entities[0].type === 'url') {
-        const urlEntity = message.entities[0];
-        const url = message.text.slice(urlEntity.offset, urlEntity.offset + urlEntity.length);
-        const urlParams = new URLSearchParams(new URL(url).search);
-        const referralId = urlParams.get('referralId');
-        
-        if (referralId) {
-            const user = ctx.message.from;
-            const telegramId = user.id;
-            const username = user.username || '';
-            const uniqueLink = username ? 
-                `${WEB_APP_BASE_URL}?telegramId=${telegramId}&username=${username}&referralId=${referralId}` :
-                `${WEB_APP_BASE_URL}?telegramId=${telegramId}&referralId=${referralId}`;
-            ctx.reply('Добро пожаловать в магический мир Криптоленда! Чтобы начать путешествие, нажмите на кнопку «Магия»!', 
-                Markup.keyboard([
-                    Markup.button.webApp('Магия!', uniqueLink)
-                ]).resize()
-            );
-        }
-    }
-});
-
 bot.launch();
 console.log('Бот запущен');
-
-
-
-
