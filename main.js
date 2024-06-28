e2e1bcaf2b1718889ee741dc20a875d3553e9587
Kirill Guzovskiy <kirillguzovskiy@MacBook-Pro-Kirill-2.local>
2024-06-28 14:45:03 +0300

    Обновление проекта

--- main.js
+++ main.js
@@ -1,30 +1,30 @@
 import { Telegraf, Markup } from 'telegraf';
+import { FirebaseService } from './firebaseService';
 
 // Инициализация Telegram бота
 const bot = new Telegraf('6979302839:AAFn6yPdRu17MwLv4A40JAgueZznSQJ6ShE');
 const WEB_APP_BASE_URL = 'https://tgappbrighfairy.web.app';
+const firebaseService = new FirebaseService();
 
 // Обработка команды /start
-bot.start((ctx) => {
+bot.start(async (ctx) => {
     const user = ctx.message.from;
     const telegramId = user.id;
     const username = user.username || '';
-    
-    // Извлечение startapp параметра
-    const startapp = ctx.message.text.split(' ')[1];
-    const referralIdMatch = startapp ? startapp.match(/^referralId_(\d+)$/) : null;
-    const referralId = referralIdMatch ? referralIdMatch[1] : '';
+
+    // Извлечение start параметра
+    const startParam = ctx.message.text.split(' ')[1];
+    const referralId = startParam ? startParam : '';
 
     // Генерация уникальной ссылки
-    let uniqueLink;
+    let uniqueLink = `${WEB_APP_BASE_URL}?telegramId=${telegramId}&username=${username}`;
     if (referralId) {
-        uniqueLink = username ?
-            `${WEB_APP_BASE_URL}?telegramId=${telegramId}&username=${username}&referralId=${referralId}` :
-            `${WEB_APP_BASE_URL}?telegramId=${telegramId}&referralId=${referralId}`;
-    } else {
-        uniqueLink = username ?
-            `${WEB_APP_BASE_URL}?telegramId=${telegramId}&username=${username}` :
-            `${WEB_APP_BASE_URL}?telegramId=${telegramId}`;
+        uniqueLink += `&referralId=${referralId}`;
+    }
+
+    // Сохранение реферальной информации в Firebase
+    if (referralId) {
+        await firebaseService.saveReferralData(telegramId, referralId);
     }
 
     // Отправка сообщения пользователю
@@ -37,3 +37,6 @@ bot.start((ctx) => {
 
 bot.launch();
 console.log('Бот запущен');
+
+
+
